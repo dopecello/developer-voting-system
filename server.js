d2d1e9e56cf0044dbd259a7be616efb3cde00dc2
dopecello <williamlocke.cello@gmail.com>
2022-09-04 17:47:13 -0400

    established db query

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 const express = require("express");
+const mysql = require("mysql2");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "dopecello",
+    password: "Jax123!@#",
+    database: "election",
+  },
+  console.log("Connected to the election database.")
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
+
 app.use((req, res) => {
   res.status(404).end();
 });
 
 app.listen(PORT, () => {
-    console.log(`This shit is bussin on port ${PORT}`);
-  });
-  
-  
+  console.log(`This shit is bussin on port ${PORT}`);
+});
